3f66301a256bbd1ec1902a98f4850a34cd1be634
Richard Atterer <richard@atterer.org>
2005-07-05 09:43:25 +0000

    Fixes to test cases

--- src/mktemplate-test1.sh
+++ src/mktemplate-test1.sh
@@ -3,15 +3,15 @@
 # Fixed sometime after 0.7.0 release
 random 256k >in1
 cp in1 in2
-random 128k >>in1
-random 128k >>in2
-random 128k >image
+random 1k >>in1
+random 1k >>in2
+random 1k >image
 cat in1 >>image
-random 128k >>image
+random 1k >>image
 mt in*
 tlist <<EOF
-in-template            0       131072
-need-file         131072       393216 DC0EKqVrguQBW6nKp-TlHQ bqRXnKaA3-Y
-in-template       524288       131072
-image-info        655360              k0Hs013jj1tATexvCT9xyA 1024
+in-template            0         1024
+need-file           1024       263168 wurT473rMMQ7z8-AXpV-EA bqRXnKaA3-Y
+in-template       264192         1024
+image-info        265216              n4TcazH7_MH5aBJhesK0tQ 1024
 EOF
