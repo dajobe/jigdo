4e98b412f51e6d1122a7681d78c0c8eadfd40fdd
Richard Atterer <richard@atterer.org>
2005-10-15 21:33:35 +0000

    *** empty log message ***

--- src/job/makeimagedl.hh
+++ src/job/makeimagedl.hh
@@ -220,7 +220,7 @@ public:
       &lt;, &gt; &amp; */
   void imageInfo(string* output, bool escapedText,
                  const char* subst[13]) const;
-  /** Helper enum for the offsets above */
+  /** Helper enum for the offsets above. E.g. TT is <tt>, TT_ is </tt> */
   enum { B, B_, I, I_, TT, TT_, U, U_, BIG, BIG_, SMALL, SMALL_, BR };
 
   /** Return ImageInfo as it appears in the .jigdo file. The value has *not*
